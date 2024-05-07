86ff38de26606b71a10cf6d7ec1c6cb2627d3169
fumiyasac <just1factory@gmail.com>
2024-05-08 00:26:22 +0900

    initial commit

---
+++ ScrollableTabActionExample/ScrollableTabActionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollableTabActionExampleApp.swift
+//  ScrollableTabActionExample
+//
+//  Created by 酒井文也 on 2024/05/08.
+//
+
+import SwiftUI
+
+@main
+struct ScrollableTabActionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
