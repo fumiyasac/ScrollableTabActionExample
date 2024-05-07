2a84d9e1203fa428c6599d9f1959b4fed94d7c76
fumiyasac <just1factory@gmail.com>
2024-05-08 01:07:36 +0900

    Extensionの追加

---
+++ ScrollableTabActionExample/Application/Extension/StringExtension.swift
@@ -0,0 +1,21 @@
+//
+//  StringExtension.swift
+//  ScrollableTabActionExample
+//
+//  Created by 酒井文也 on 2024/05/08.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+
+    // MARK: - Function
+
+    // MEMO: 設定されたUIFontの値から幅を取得する（※Tab型切り替えMenu画面等で活用する）
+    func widthOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.width
+    }
+}
