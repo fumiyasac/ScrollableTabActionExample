1edae2867dfc49955b557109cfed2c6101b40661
fumiyasac <just1factory@gmail.com>
2024-05-17 12:35:34 +0900

    APIクライアントの追加

---
+++ ScrollableTabActionExample/Application/Screen/PremiumPosterScreen/PremiumPosterComponents/PremiumPosterThumbnailView.swift
@@ -0,0 +1,24 @@
+//
+//  PremiumPosterThumbnailView.swift
+//  ScrollableTabActionExample
+//
+//  Created by 酒井文也 on 2024/05/17.
+//
+
+import SwiftUI
+
+struct PremiumPosterThumbnailView: View {
+
+    // MARK: - Body
+
+    var body: some View {
+        // TODO: コンテンツ要素用のView要素を作成する
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    PremiumPosterThumbnailView()
+}
