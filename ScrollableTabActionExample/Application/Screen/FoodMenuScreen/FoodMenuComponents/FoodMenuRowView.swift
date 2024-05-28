aeede02e5de8f0a1c52c23d75221814d3e8a97f9
fumiyasac <just1factory@gmail.com>
2024-05-28 22:44:45 +0900

    部品を作成する

---
+++ ScrollableTabActionExample/Application/Screen/FoodMenuScreen/FoodMenuComponents/FoodMenuRowView.swift
@@ -0,0 +1,23 @@
+//
+//  FoodMenuRowView.swift
+//  ScrollableTabActionExample
+//
+//  Created by 酒井文也 on 2024/05/28.
+//
+
+import SwiftUI
+
+struct FoodMenuRowView: View {
+
+    // MARK: - Body
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    FoodMenuRowView()
+}
