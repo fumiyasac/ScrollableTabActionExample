9a7b1f45364c83118b448beda1681bf5acc7341b
fumiyasac <just1factory@gmail.com>
2024-06-21 09:17:01 +0900

    Requestクラスを作成

---
+++ ScrollableTabActionExample/Application/Request/PremiumPosterRequest.swift
@@ -0,0 +1,12 @@
+//
+//  PremiumPosterRequest.swift
+//  ScrollableTabActionExample
+//
+//  Created by 酒井文也 on 2024/06/21.
+//
+
+import Foundation
+
+protocol PremiumPosterRequest {
+    func getAll() async throws -> [PremiumPosterModel]
+}
