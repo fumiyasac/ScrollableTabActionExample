fc61f1aa3efc8bcc251336b7aa6576f003f70487
fumiyasac <just1factory@gmail.com>
2024-06-22 00:54:21 +0900

    取り急ぎHeader用View要素を設定する

---
+++ ScrollableTabActionExample/Application/Screen/FoodMenuScreen/FoodMenuComponents/FoodMenuHeaderView.swift
@@ -0,0 +1,23 @@
+//
+//  FoodMenuHeaderView.swift
+//  ScrollableTabActionExample
+//
+//  Created by 酒井文也 on 2024/06/22.
+//
+
+import SwiftUI
+
+struct FoodMenuHeaderView: View {
+
+    // MARK: - Body
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    FoodMenuHeaderView()
+}
