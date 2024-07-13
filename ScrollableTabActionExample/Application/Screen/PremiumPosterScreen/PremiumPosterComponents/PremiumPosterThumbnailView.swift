a58c254b5dbc4c6541138c8c766d242706bfde45
fumiyasac <just1factory@gmail.com>
2024-07-14 05:57:29 +0900

    コメントを追記

--- ScrollableTabActionExample/Application/Screen/PremiumPosterScreen/PremiumPosterComponents/PremiumPosterThumbnailView.swift
+++ ScrollableTabActionExample/Application/Screen/PremiumPosterScreen/PremiumPosterComponents/PremiumPosterThumbnailView.swift
@@ -13,6 +13,7 @@ struct PremiumPosterThumbnailView: View {
 
     var body: some View {
         // TODO: コンテンツ要素用のView要素を作成する
+        // 雑誌アプリの様に一覧で表示する想定
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
