cbcbe8cee2851166cbbd3691756932b8c06a2cf9
fumiyasac <just1factory@gmail.com>
2024-12-13 13:15:55 +0900

    ちょっとした修正を実施する

--- ScrollableTabActionExample/Application/Screen/FoodMenuScreen/FoodMenuComponents/FoodMenuHeaderView.swift
+++ ScrollableTabActionExample/Application/Screen/FoodMenuScreen/FoodMenuComponents/FoodMenuHeaderView.swift
@@ -12,7 +12,7 @@ struct FoodMenuHeaderView: View {
     // MARK: - Body
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("FoodMenuHeaderView")
     }
 }
 
--- ScrollableTabActionExample/Application/Screen/FoodMenuScreen/FoodMenuComponents/FoodMenuRowView.swift
+++ ScrollableTabActionExample/Application/Screen/FoodMenuScreen/FoodMenuComponents/FoodMenuRowView.swift
@@ -12,7 +12,7 @@ struct FoodMenuRowView: View {
     // MARK: - Body
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("FoodMenuRowView")
     }
 }
 
