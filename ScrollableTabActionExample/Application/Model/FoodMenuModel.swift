80ea9b0afab127b0cc59e94dd73bc92f73ac8370
fumiyasac <just1factory@gmail.com>
2024-12-16 18:18:02 +0900

    FoodMenuModelをDecodableとHashableに準拠する

--- ScrollableTabActionExample/Application/Model/FoodMenuModel.swift
+++ ScrollableTabActionExample/Application/Model/FoodMenuModel.swift
@@ -7,19 +7,17 @@
 
 import Foundation
 
-struct FoodMenuModel: Identifiable {
+struct FoodMenuModel: Hashable, Decodable {
 
     // MARK: - Property
 
-    let id: UUID = UUID()
-    let foodID: Int
+    let id: Int
     let name: String
     let category: FoodMenuModel.FoodMenuCategeory
     let dish: String
-    let subTitle: String
     let price: Int
     let kcal: Int
-    let foodUrl: URL?
+    let foodUrl: String
 
     // MARK: - Enum
 
@@ -31,5 +29,52 @@ struct FoodMenuModel: Identifiable {
         case sushi = "🍣お寿司"
         case western = "🍔洋食"
         case japanese = "🍲和食"
+    }
+
+    // MARK: - Enum
+
+    private enum Keys: String, CodingKey {
+        case id
+        case name
+        case category
+        case dish
+        case price
+        case kcal
+        case foodUrl = "food_url"
+    }
+
+    // MARK: - Initializer
+
+    init(
+        id: Int,
+        name: String,
+        category: String,
+        dish: String,
+        price: Int,
+        kcal: Int,
+        foodUrl: String
+    ) {
+        self.id = id
+        self.name = name
+        self.category = FoodMenuModel.FoodMenuCategeory(rawValue: category) ?? .sushi
+        self.dish = dish
+        self.price = price
+        self.kcal = kcal
+        self.foodUrl = foodUrl
+    }
+
+    init(from decoder: Decoder) throws {
+
+        // JSONの配列内の要素を取得する
+        let container = try decoder.container(keyedBy: Keys.self)
+
+        // JSONの配列内の要素にある値をDecodeして初期化する
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.category = FoodMenuModel.FoodMenuCategeory(rawValue: try container.decode(String.self, forKey: .category)) ?? .sushi
+        self.dish = try container.decode(String.self, forKey: .dish)
+        self.price = try container.decode(Int.self, forKey: .price)
+        self.kcal = try container.decode(Int.self, forKey: .kcal)
+        self.foodUrl = try container.decode(String.self, forKey: .foodUrl)
     }
 }
